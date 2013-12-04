f5877540e1f5dc2ace1923bce20ea0686b0273b3
Mehmet Tamturk <mehmettamturk@gmail.com>
2013-12-04 22:43:42 +0200

    Initial commit.

---
+++ assets/www/data.js
@@ -0,0 +1,151 @@
+var times = [{
+    day: 1,
+    date: '9 July',
+    imsak: new Date('July 9, 2013 03:35:00'),
+    iftar: new Date('July 9, 2013 20:47:00')
+},{
+    day: 2,
+    date: '10 July',
+    imsak: new Date('July 10, 2013 03:36:00'),
+    iftar: new Date('July 10, 2013 20:47:00')
+},{
+    day: 3,
+    date: '11 July',
+    imsak: new Date('July 11, 2013 03:37:00'),
+    iftar: new Date('July 11, 2013 20:46:00')
+},{
+    day: 4,
+    date: '12 July',
+    imsak: new Date('July 12, 2013 03:38:00'),
+    iftar: new Date('July 12, 2013 20:46:00')
+},{
+    day: 5,
+    date: '13 July',
+    imsak: new Date('July 13, 2013 03:40:00'),
+    iftar: new Date('July 13, 2013 20:45:00')
+},{
+    day: 6,
+    date: '14 July',
+    imsak: new Date('July 14, 2013 03:41:00'),
+    iftar: new Date('July 14, 2013 20:45:00')
+},{
+    day: 7,
+    date: '15 July',
+    imsak: new Date('July 15, 2013 03:42:00'),
+    iftar: new Date('July 15, 2013 20:44:00')
+},{
+    day: 8,
+    date: '16 July',
+    imsak: new Date('July 16, 2013 03:43:00'),
+    iftar: new Date('July 16, 2013 20:44:00')
+},{
+    day: 9,
+    date: '17 July',
+    imsak: new Date('July 17, 2013 03:45:00'),
+    iftar: new Date('July 17, 2013 20:43:00')
+},{
+    day: 10,
+    date: '18 July',
+    imsak: new Date('July 18, 2013 03:46:00'),
+    iftar: new Date('July 18, 2013 20:42:00')
+},{
+    day: 11,
+    date: '19 July',
+    imsak: new Date('July 19, 2013 03:47:00'),
+    iftar: new Date('July 19, 2013 20:42:00')
+},{
+    day: 12,
+    date: '20 July',
+    imsak: new Date('July 20, 2013 03:49:00'),
+    iftar: new Date('July 20, 2013 20:41:00')
+},{
+    day: 13,
+    date: '21 July',
+    imsak: new Date('July 21, 2013 03:50:00'),
+    iftar: new Date('July 21, 2013 20:40:00')
+},{
+    day: 14,
+    date: '22 July',
+    imsak: new Date('July 22, 2013 03:52:00'),
+    iftar: new Date('July 22, 2013 20:39:00')
+},{
+    day: 15,
+    date: '23 July',
+    imsak: new Date('July 23, 2013 03:53:00'),
+    iftar: new Date('July 23, 2013 20:39:00')
+},{
+    day: 16,
+    date: '24 July',
+    imsak: new Date('July 24, 2013 03:55:00'),
+    iftar: new Date('July 24, 2013 20:38:00')
+},{
+    day: 17,
+    date: '25 July',
+    imsak: new Date('July 25, 2013 03:56:00'),
+    iftar: new Date('July 25, 2013 20:37:00')
+},{
+    day: 18,
+    date: '26 July',
+    imsak: new Date('July 26, 2013 03:58:00'),
+    iftar: new Date('July 26, 2013 20:36:00')
+},{
+    day: 19,
+    date: '27 July',
+    imsak: new Date('July 27, 2013 03:59:00'),
+    iftar: new Date('July 27, 2013 20:35:00')
+},{
+    day: 20,
+    date: '28 July',
+    imsak: new Date('July 28, 2013 04:01:00'),
+    iftar: new Date('July 28, 2013 20:34:00')
+},{
+    day: 21,
+    date: '29 July',
+    imsak: new Date('July 29, 2013 04:02:00'),
+    iftar: new Date('July 29, 2013 20:33:00')
+},{
+    day: 22,
+    date: '30 July',
+    imsak: new Date('July 30, 2013 04:04:00'),
+    iftar: new Date('July 30, 2013 20:32:00')
+},{
+    day: 23,
+    date: '31 July',
+    imsak: new Date('July 31, 2013 04:05:00'),
+    iftar: new Date('July 31, 2013 20:31:00')
+},{
+    day: 24,
+    date: '1 August',
+    imsak: new Date('August 1, 2013 04:07:00'),
+    iftar: new Date('August 1, 2013 20:30:00')
+},{
+    day: 25,
+    date: '2 August',
+    imsak: new Date('August 2, 2013 04:08:00'),
+    iftar: new Date('August 2, 2013 20:29:00')
+},{
+    day: 26,
+    date: '3 August',
+    imsak: new Date('August 3, 2013 04:10:00'),
+    iftar: new Date('August 3, 2013 20:28:00')
+},{
+    day: 27,
+    date: '4 August',
+    imsak: new Date('August 4, 2013 04:11:00'),
+    iftar: new Date('August 4, 2013 20:27:00')
+},{
+    day: 28,
+    date: '5 August',
+    imsak: new Date('August 5, 2013 04:13:00'),
+    iftar: new Date('August 5, 2013 20:25:00')
+},{
+    day: 29,
+    date: '6 August',
+    imsak: new Date('August 6, 2013 04:14:00'),
+    iftar: new Date('August 6, 2013 20:24:00')
+},{
+    day: 30,
+    date: '7 August',
+    imsak: new Date('August 7, 2013 04:16:00'),
+    iftar: new Date('August 7, 2013 20:23:00')
+}];
